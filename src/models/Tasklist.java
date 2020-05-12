2853aad0f73b3e67cd5a1dd87a7505b20511b463
hibiki.kishino <h_kishino@allexceed.co.jp>
2020-05-12 15:44:41 +0900

    Add pagination

--- src/models/Tasklist.java
+++ src/models/Tasklist.java
@@ -16,7 +16,12 @@ import javax.persistence.Table;
     @NamedQuery(
             name = "getAllTasklist",
             query = "SELECT m FROM Tasklist AS m ORDER BY m.id DESC"
+            ),
+    @NamedQuery(
+            name = "getTasklistCount",
+            query = "SELECT COUNT(m) FROM Tasklist AS m"
             )
+
 })
 @Table(name = "tasks")
 public class Tasklist {
